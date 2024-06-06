0af94ffa827c83c039ee83a695c33c665c8d597c
joanneoliviaa <joanneolivia81@gmail.com>
2024-06-06 13:17:59 +0700

    aaa

--- storage/framework/views/e06cdd887dc3b5d1597139c317f6ccb0.php
+++ storage/framework/views/e06cdd887dc3b5d1597139c317f6ccb0.php
@@ -8,26 +8,18 @@
 <?php $attributes = $attributes->except(\App\View\Components\AppLayout::ignoredParameterNames()); ?>
 <?php endif; ?>
 <?php $component->withAttributes([]); ?>
-     <?php $__env->slot('header', null, []); ?> 
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            <?php echo e(__('Timeline')); ?>
-
-        </h2>
-     <?php $__env->endSlot(); ?>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <form action="<?php echo e(route('post.store')); ?>" method="post">
+                   <form action="/posts" class="form-control" method="post">
                         <?php echo csrf_field(); ?>
-                        <?php echo e(__("Selamat datang di medsos kelompok 2 back end ayeea ayyeeaaa")); ?>
-
-                        <textarea id = "message" class="w-full rounded textarea textarea-bordered" rows="3"
-                        placeholder="Post something..."></textarea>
-                        <Input type="submit" value="post, ini kayaknya ada yang salah" class="button">
-                        <input type="hidden" name="_token" value="<?php echo e(csrf_token()); ?>" />
-                    </form>
+                        <textarea class="textarea textarea-bordered mb-2"  cols="30" id="" 
+                        name="content" placeholder="Tuliskan Sesuatu..."
+                            rows="3"></textarea>
+                            <input type="submit" value="Post" class="btn btn-secondary">
+                   </form>
                 </div>
             </div>
         </div>
@@ -41,5 +33,4 @@
 <?php if (isset($__componentOriginal9ac128a9029c0e4701924bd2d73d7f54)): ?>
 <?php $component = $__componentOriginal9ac128a9029c0e4701924bd2d73d7f54; ?>
 <?php unset($__componentOriginal9ac128a9029c0e4701924bd2d73d7f54); ?>
-<?php endif; ?>
-<?php /**PATH /Applications/XAMPP/xamppfiles/htdocs/UAS/UAS-BACKEND/resources/views/dashboard.blade.php ENDPATH**/ ?>
+<?php endif; ?><?php /**PATH /Applications/XAMPP/xamppfiles/htdocs/UAS/UAS-BACKEND/resources/views/dashboard.blade.php ENDPATH**/ ?>
