ba49daa016ae3910f56543228b7104ce1b09f60e
Galang <162521324+IGALAN9@users.noreply.github.com>
2024-06-13 11:56:06 +0700

    progress 2

--- app/Http/Controllers/Post_LikeController.php
+++ app/Http/Controllers/Post_LikeController.php
@@ -8,11 +8,19 @@ use Illuminate\Http\Request;
 class Post_LikeController extends Controller
 {
     public function like(Posting $posting){
+        $liker = auth()->user();
 
+        $liker->likes()->attach($posting);
+
+        return redirect()->route('dashboard')->with('success',"liked successfully!");
     }
 
     public function unlike(Posting $posting){
+        $liker = auth()->user();
 
+        $liker->likes()->deattach($posting);
+
+        return redirect()->route('dashboard')->with('success',"liked successfully!");
     }
 }
 
