c1a1220b2476cb9bf6f8faa8f2d9fbe64666020f
Kenychee <khenichi.535230142@stu.untar.ac.id>
2024-06-23 18:30:56 +0700

    Komentar, Bookmark

--- resources/views/postings/show.blade.php
+++ resources/views/postings/show.blade.php
@@ -127,10 +127,40 @@
             display: flex;
             flex-direction: column;
         }
+
+        /**back button */
+        .nav-link {
+            display: flex;
+            justify-content: flex-start;
+            margin-bottom: 1rem;
+            margin-left: 124px;
+        }
+
+        .nav-link button {
+            background-color: #4a5568;
+            color: #fff;
+            border: none;
+            padding: 0.5rem 1rem;
+            border-radius: 0.375rem;
+            cursor: pointer;
+            font-size: 1rem;
+            transition: background-color 0.3s;
+        }
+
+        .nav-link button:hover {
+            background-color: #2d3748;
+        }
     </style>
 </head>
 <body>
     <div class="py-12">
+
+        <div class="nav-link hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+            <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                <button>{{ __('< Back') }}</button>
+            </x-nav-link>
+        </div>
+
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8 space-y-4">
             <div class="card card-bordered bg-yellow-100 mb-2">
                 <div class="card-body">
